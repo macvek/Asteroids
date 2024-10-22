b7b2a0a7ba7602589b7f194b2d4de98f290b46dd
Maciej Aleksandrowicz <macvek@gmail.com>
2024-10-22 10:57:52 +0200

    Very inefficient matrix operations

--- P3/P3.cpp
+++ P3/P3.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <math.h>
 #include <SDL.h>
 #include <SDL_mixer.h>
 
@@ -9,9 +10,15 @@ using namespace std;
 
 Uint32 globalCustomEventId = 0;
 
+int worldFrame = 0;
 
-Uint32 tickMe(Uint32 interval, void* param) {
-	cout << "Tick" << endl;
+
+Uint32 tickFrame(Uint32 interval, void* param) {
+	++worldFrame;
+	if (worldFrame % 30 == 0) {
+		cout << "FRAME: " << worldFrame << endl;
+	}
+	
 	SDL_Event e = {};
 	e.type = globalCustomEventId;
 
@@ -19,12 +26,108 @@ Uint32 tickMe(Uint32 interval, void* param) {
 	return interval;
 }
 
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+typedef struct M33 {
+	double m[3][3];
+} M33;
+
+void M33_Identity(M33& m) {
+	m.m[0][0] = 1; m.m[0][1] = 0; m.m[0][2] = 0;
+	m.m[1][0] = 0; m.m[1][1] = 1; m.m[1][2] = 0;
+	m.m[2][0] = 0; m.m[2][1] = 0; m.m[2][2] = 1;
+}
+
+void M33_Rotate(M33& m, double phi) {
+	m.m[0][0] = cos(phi); m.m[0][1] = -sin(phi); m.m[0][2] = 0;
+	m.m[1][0] = sin(phi); m.m[1][1] =  cos(phi); m.m[1][2] = 0;
+	m.m[2][0] = 0;		  m.m[2][1] = 0;		 m.m[2][2] = 1;
+}
+
+void M33_Translate(M33& m, double x, double y) {
+	m.m[0][0] = 1; m.m[0][1] = 0; m.m[0][2] = x;
+	m.m[1][0] = 0; m.m[1][1] = 1; m.m[1][2] = y;
+	m.m[2][0] = 0; m.m[2][1] = 0; m.m[2][2] = 1;
+}
+
+void M33_Copy(M33& s, M33& d) {
+	for (int y = 0; y < 3; ++y)
+		for (int x = 0; x < 3; ++x)
+			d.m[y][x] = s.m[y][x];
+}
+
+void M33_Mult(M33& s, M33& o) {
+	M33 r;
+	for (int l = 0; l < 3; ++l) {
+		r.m[l][0] = s.m[l][0] * o.m[0][0] + s.m[l][1] * o.m[1][0] + s.m[l][2] * o.m[2][0];
+		r.m[l][1] = s.m[l][0] * o.m[0][1] + s.m[l][1] * o.m[1][1] + s.m[l][2] * o.m[2][1];
+		r.m[l][2] = s.m[l][0] * o.m[0][2] + s.m[l][1] * o.m[1][2] + s.m[l][2] * o.m[2][2];
+	}
+
+	M33_Copy(r, s);
+}
+
+
+
+SDL_Point M33_Apply(M33& m, SDL_Point& p) {
+	int nX = m.m[0][0] * p.x + m.m[0][1] * p.y + m.m[0][2];
+	int nY = m.m[1][0] * p.x + m.m[1][1] * p.y + m.m[1][2];
+
+	return SDL_Point{ nX, nY };
+}
+
+void M33_Print(M33& m) {
+	cout << " [ " << m.m[0][0] << "\t" << m.m[0][1] << "\t" << m.m[0][2] << " ] " << endl;
+	cout << " [ " << m.m[1][0] << "\t" << m.m[1][1] << "\t" << m.m[1][2] << " ] " << endl;
+	cout << " [ " << m.m[2][0] << "\t" << m.m[2][1] << "\t" << m.m[2][2] << " ] " << endl;
+}
+
+bool printOnce = true;
+void renderFrame() {
+
+
+	if (SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE)) {
+		cout << "SDL_SetRenderDrawColor: " << SDL_GetError() << endl;
+	}
+
+	if (SDL_RenderClear(renderer)) {
+		cout << "SDL_RenderClear: " << SDL_GetError() << endl;
+	}
+	SDL_Rect rect;
+	
+	M33 base;	M33_Identity(base);
+	M33 rotate; M33_Rotate(rotate, M_PI + 0.01*worldFrame);
+	M33 t;		M33_Translate(t, 500, 500);
+	
+	if (printOnce) M33_Print(base);
+	M33_Mult(base, t);
+	M33_Mult(base, rotate);
+	if (printOnce) 	M33_Print(base);
+
+	printOnce = false;
+	//
+
+	
+
+	SDL_Point points[] = {
+		{-50,-50}, {50,-50}, {50,50}, {-50,50}
+	};
+	
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+
+	for (int i = 0; i < 4; i++) {
+		SDL_Point from = M33_Apply(base, points[i]);
+		SDL_Point to = M33_Apply(base, points[(i+1) % 4]);
+
+		SDL_RenderDrawLine(renderer, from.x, from.y, to.x, to.y);
+	}
+
+	SDL_RenderPresent(renderer);
+}
+
 int main(int argc, char* argv[])
 {
-	SDL_Surface* winSurface = NULL;
-	SDL_Window* window = NULL;
-	SDL_Renderer* renderer = NULL;
-
 	// Initialize SDL. SDL_Init will return -1 if it fails.
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
 		cout << "Error initializing SDL: " << SDL_GetError() << endl;
@@ -38,77 +141,23 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	int registeredTimer = SDL_AddTimer(1000, tickMe, nullptr);
+	int registeredTimer = SDL_AddTimer(15, tickFrame, nullptr);
 	cout << "Registered timer at: " << registeredTimer << endl;
+	
+	window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, 0);
 
-	// Create our window
-	if (true) {
-		window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, 0);
-	}
-	else {
-		if (SDL_CreateWindowAndRenderer(1280, 720, 0, &window, &renderer)) {
-			cout << "SDL_CreateWindowAndRenderer: " << SDL_GetError() << endl;
-		}
-	}
-
-	// Make sure creating the window succeeded
-	if (!window) {
+	if (nullptr == window) {
 		cout << "Error creating window: " << SDL_GetError() << endl;
-		// End the program
 		return 1;
 	}
 
-	if (false) {
-		// Get the surface from the window
-		winSurface = SDL_GetWindowSurface(window);
-
-		// Make sure getting the surface succeeded
-		if (!winSurface) {
-			cout << "Error getting surface: " << SDL_GetError() << endl;
-			// End the program
-			return 1;
-		}
-
-		// Fill the window with a white rectangle
-		SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 255, 255, 255));
-
-		// Update the window display
-		SDL_UpdateWindowSurface(window);
-	}
-
-
-	if (true) {
-		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-	}
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
 	if (nullptr == renderer) {
 		cout << "Error getting renderer: " << SDL_GetError() << endl;
+		return 1;
 	}
-	else {
-		if (SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE)) {
-			cout << "SDL_SetRenderDrawColor: " << SDL_GetError() << endl;
-		}
-
-		if (SDL_RenderClear(renderer)) {
-			cout << "SDL_RenderClear: " << SDL_GetError() << endl;
-		}
-		SDL_Rect rect;
-		rect.x = 0;
-		rect.y = 0;
-		rect.w = 100;
-		rect.h = 100;
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-		
-		if (SDL_RenderFillRect(renderer, &rect)) {
-			cout << "SDL_RenderFillRect: " << SDL_GetError() << endl;
-		}
-		if (SDL_RenderFlush(renderer)) {
-			cout << "SDL_RenderFlush: " << SDL_GetError() << endl;
-		}
-
-		SDL_RenderDrawLine(renderer, 100, 100, 200, 200);
-
-		SDL_RenderPresent(renderer);
-	}
+	
+	renderFrame();
 	int audio_rate = MIX_DEFAULT_FREQUENCY;
 	Uint16 audio_format = MIX_DEFAULT_FORMAT;
 	int audio_channels = MIX_DEFAULT_CHANNELS;
@@ -124,18 +173,20 @@ int main(int argc, char* argv[])
 	else {
 		Mix_QuerySpec(&audio_rate, &audio_format, &audio_channels);
 		cout << "Mix_QuerySpec: " << audio_rate <<" "<< audio_format<<" "<< audio_channels << endl;
-
 	}
 
 	bool musicStarted = false;
 	bool paused = false;
 
-	// Wait
-	while (true) {
-		SDL_Event e;
+	SDL_Event e;
+	for (;;) {
 		if (SDL_WaitEvent(&e)) {
 			if (e.type == SDL_KEYDOWN /*|| e.type == SDL_KEYUP*/) {
 				SDL_KeyboardEvent* keyboardEvent = (SDL_KeyboardEvent*)(&e);
+				if (keyboardEvent->keysym.scancode == SDL_SCANCODE_ESCAPE) {
+					break;
+				}
+				
 				if (keyboardEvent->keysym.scancode == SDL_SCANCODE_RETURN) {
 					cout << "Return key pressed" << endl;
 
@@ -153,21 +204,21 @@ int main(int argc, char* argv[])
 
 						paused = !paused;
 					}
-					
-					
 				}
+				renderFrame();
 			}
 			if (e.type == SDL_QUIT) {
 				break;
 			}
 		}
 		if (e.type == globalCustomEventId) {
-			cout << "Custom event triggered" << endl;
+			renderFrame();
 		}
-		cout << "Event type: " << e.type << endl;
+		else {
+			cout << "Event type: " << e.type << endl;
+		}
 	}
 
-	// Destroy the window. This will also destroy the surface
 	SDL_DestroyWindow(window);
 
 	// Quit SDL
